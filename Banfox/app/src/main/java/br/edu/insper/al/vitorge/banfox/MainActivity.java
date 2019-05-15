3ff93a46bc0b0db35f3128f1fe7b3ba52ddcc09f
VFermat <vitor.eller30@gmail.com>
2019-05-14 21:23:31 -0300

    General updates

--- Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/MainActivity.java
+++ Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/MainActivity.java
@@ -1,13 +1,53 @@
 package br.edu.insper.al.vitorge.banfox;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location clientLocation;
+
+    private static final int REQUEST_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        /*
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        clientLocation = location;
+                        Log.d("message", String.valueOf(clientLocation));
+                    }
+                }
+            });
+
+        }  else {
+            String[] permissions = new String[]{
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+            };
+            ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_LOCATION);
+        }
+        */
+
     }
 }
