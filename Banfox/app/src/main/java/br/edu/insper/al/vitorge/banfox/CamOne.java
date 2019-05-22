6a0d1f66d58184fb11a90449f05d6116a6ce35af
Henry Rocha <HenryRocha05@Hotmail.com>
2019-05-21 23:30:17 -0300

    Atualizando repo.

--- Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/CamOne.java
+++ Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/CamOne.java
@@ -71,6 +71,9 @@ public class CamOne extends AppCompatActivity {
     private Handler mBackgroundHandler;
     private HandlerThread mBackgroundThread;
 
+    int photo;
+    byte[] source, target;
+    CompareFaces faceCompare = new CompareFaces();
 
     CameraDevice.StateCallback stateCallBack = new CameraDevice.StateCallback() {
         @Override
@@ -107,9 +110,12 @@ public class CamOne extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 takePicture();
-
-                Intent intent = new Intent(CamOne.this, LoadingInformations.class);
-                startActivity(intent);
+                photo += 1;
+//                Intent intent = new Intent(CamOne.this, LoadingInformations.class);
+//                startActivity(intent);
+                if (photo >= 2) {
+                    faceCompare.compareFaces(source, target);
+                }
             }
         });
     }
@@ -158,6 +164,12 @@ public class CamOne extends AppCompatActivity {
                             byte[] bytes = new byte[buffer.capacity()];
                             buffer.get(bytes);
                             save(bytes);
+
+                            if (photo == 1) {
+                                source = bytes;
+                            } else {
+                                target = bytes;
+                            }
                         }
                         catch (FileNotFoundException e){
                             e.printStackTrace();
