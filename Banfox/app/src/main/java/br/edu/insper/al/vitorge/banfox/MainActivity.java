b351dc62db6bffadb7c376b7dc3f210e0b33909d
VFermat <vitor.eller30@gmail.com>
2019-05-27 13:28:46 -0300

    update on permissions request

--- Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/MainActivity.java
+++ Banfox/app/src/main/java/br/edu/insper/al/vitorge/banfox/MainActivity.java
@@ -1,6 +1,7 @@
 package br.edu.insper.al.vitorge.banfox;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
@@ -21,7 +22,13 @@ public class MainActivity extends AppCompatActivity {
     private FusedLocationProviderClient fusedLocationClient;
     private Location clientLocation;
 
-    private static final int REQUEST_LOCATION = 0;
+    private static final int REQUEST_PERMISSIONS = 0;
+    private static final String[] permissions = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,31 +39,38 @@ public class MainActivity extends AppCompatActivity {
 
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
-        }
+        askPermissions(this, permissions);
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    if (location != null) {
-                        clientLocation = location;
-                        ((Global) MainActivity.this.getApplication()).setUserLocation(clientLocation);
-                        Log.d("message", String.valueOf(clientLocation));
-                    }
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    clientLocation = location;
+                    ((Global) MainActivity.this.getApplication()).setUserLocation(clientLocation);
+                    Log.d("message", String.valueOf(clientLocation));
                 }
-            });
-
-        }  else {
-            String[] permissions = new String[]{
-                    Manifest.permission.ACCESS_COARSE_LOCATION,
-            };
-            ActivityCompat.requestPermissions(MainActivity.this, permissions, REQUEST_LOCATION);
-        }
+            }
+        });
 
         Button button = findViewById(R.id.home_btn);
         button.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, ExplanationActivity.class)));
     }
+
+    public static boolean checkPermissions(Context context, String... permissions) {
+        if (context != null && permissions != null) {
+            for (String permission : permissions) {
+                if (ActivityCompat.checkSelfPermission(context, permission) != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void askPermissions(Context context, String... permissions) {
+        if (!checkPermissions(context, permissions)) {
+            ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSIONS);
+        }
+    }
+
 }
